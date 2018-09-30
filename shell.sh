707248aade7e2fbd33420fe388edf1bf3ad488b1
unknown <drinow@sina.com>
2018-09-30 18:11:48 +0800

    fix

--- shell.sh
+++ shell.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+echo "-------------------------"
 echo "start shell"
-git status&&git add .&&git commit -m "fix"
+git status&&git add .&&git commit -m "fix"&&
+echo "-------------------------"
+echo "end shell"
